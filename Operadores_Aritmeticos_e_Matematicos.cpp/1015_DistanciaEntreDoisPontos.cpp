aaf10f32bacd8031ef9123a8941e2622cc9f3384
yasminvasconceloss <yasminvasconcelos59@gmail.com>
2023-04-04 08:02:02 -0300

    1015

---
+++ Operadores_Aritmeticos_e_Matematicos.cpp/1015_DistanciaEntreDoisPontos.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+ 
+int main() { 
+
+    double x1,y1,x2,y2,distancia;
+
+    cin >> x1;
+    cin >> y1;
+    cin >> x2;
+    cin >> y2;
+
+
+    distancia = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+    cout << fixed << setprecision(4);
+    cout << distancia << endl;
+
+    return 0;
+}
